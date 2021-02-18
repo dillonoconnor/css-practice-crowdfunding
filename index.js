ae0404582ed09de53ea97b5bc141d3fcdb3e769d
Dillon O'Connor <dilong818@gmail.com>
2021-02-17 23:09:24 -0500

    add project backing modal option selectors

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 let modal = document.querySelector(".modal");
+let circles = document.querySelectorAll(".circle")
 let trigger = document.querySelector(".trigger");
 let closeButton = document.querySelector(".close-button")
 
 const toggleModal = () => {
-  modal.classList.toggle("show-modal")
+  modal.classList.toggle("show-modal");
+}
+
+const toggleSelection = (event) => {
+  if (!event.target.classList.contains("disabled")) {
+    event.target.classList.toggle("circle");
+    event.target.classList.toggle("filled-circle");
+  }
+  circles.forEach(circle => {
+    if (circle.id === event.target.id) {
+      return;
+    } else {
+      circle.className = "circle";
+    }
+  });
 }
 
 const windowOnClick = (event) => {
@@ -13,5 +28,6 @@ const windowOnClick = (event) => {
 }
 
 trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal)
+closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+circles.forEach(circle => circle.addEventListener("click", toggleSelection));
