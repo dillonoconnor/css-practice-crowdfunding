7bf17917e2a48f32ed348c8c2b74539d2dffe303
Dillon O'Connor <dilong818@gmail.com>
2021-02-18 23:14:02 -0500

    js for project backing radio buttons and bookmark button svg styling

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let modal = document.querySelector(".modal");
-let circles = document.querySelectorAll(".circle")
+let circles = document.querySelectorAll(".circle");
 let trigger = document.querySelector(".trigger");
-let closeButton = document.querySelector(".close-button")
+let closeButton = document.querySelector(".close-button");
+let bookmarkButton = document.querySelector("#bookmark-button");
+let bookmarkImg = document.querySelector(".grey-button-img circle");
+let bookmarkImgInner = document.querySelector(".grey-button-img path");
 
 const toggleModal = () => {
   modal.classList.toggle("show-modal");
@@ -27,7 +30,20 @@ const windowOnClick = (event) => {
   }
 }
 
+const toggleBookmarked = () => {
+  bookmarkButton.classList.toggle("bookmarked");
+  if (bookmarkImg.getAttribute("fill") === "#2F2F2F") {
+    bookmarkImg.setAttribute("fill", "hsl(176, 50%, 47%)");
+    bookmarkImgInner.setAttribute('fill', "#fff");
+  } else {
+    bookmarkImg.setAttribute("fill", "#2F2F2F");
+    bookmarkImgInner.setAttribute('fill', "#B1B1B1");
+  }
+    
+}
+
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 circles.forEach(circle => circle.addEventListener("click", toggleSelection));
+bookmarkButton.addEventListener("click", toggleBookmarked);
