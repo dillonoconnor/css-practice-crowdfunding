be17763e18119fdb3a00c5a7ced868fafba4a2b4
Dillon O'Connor <dilong818@gmail.com>
2021-02-20 10:03:18 -0500

    finish all basdic modal functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let modal = document.querySelector(".modal");
+let optionsModal = document.querySelector("#options-modal");
+let thanksModal = document.querySelector("#thanks-modal");
 let circles = document.querySelectorAll(".circle");
+let backingOptions = document.querySelectorAll("[id^=backing-option]");
 let trigger = document.querySelector(".trigger");
 let closeButton = document.querySelector(".close-button");
 let bookmarkButton = document.querySelector("#bookmark-button");
@@ -9,9 +12,15 @@ let bookmarkImgInner = document.querySelector(".grey-button-img path");
 let pledgeBoxes = document.querySelectorAll("div[id^=pledge-box]");
 let pledgeBox1 = document.querySelector("#pledge-box-1");
 let pledgeBox2 = document.querySelector("#pledge-box-2");
+let continueButtons = document.querySelectorAll("a[data='continue']");
+let okayButton = document.querySelector("#okay");
 
 const toggleModal = () => {
   modal.classList.toggle("show-modal");
+  if (optionsModal.classList.contains("hidden")) {
+    optionsModal.classList.remove("hidden");
+    thanksModal.classList.add("hidden");
+  };
 }
 
 const toggleSelection = (event) => {
@@ -19,36 +28,36 @@ const toggleSelection = (event) => {
     event.target.classList.toggle("circle");
     event.target.classList.toggle("filled-circle");
     event.target.parentNode.parentNode.classList.toggle("thick-cyan-border");
+
+    backingOptions.forEach(option => {
+      if (option.id === event.target.id || option.classList.contains("disabled")) {
+        return;
+      } else {
+        option.className = "circle";
+        option.parentNode.parentNode.classList.remove("thick-cyan-border");
+      }
+    });
+
     switch (event.target.id) {
       case "backing-option-1":
         pledgeBoxes.forEach(box => {
-          if (!box.classList.contains("hidden")) {
-            box.classList.add("hidden");
-          }
+          box.classList.add("hidden");
         });
+        break;
       case "backing-option-2":
         pledgeBox1.classList.toggle("hidden");
-        if (!pledgeBox2.hasClass("hidden")) {
+        if (!pledgeBox2.classList.contains("hidden")) {
           pledgeBox2.classList.toggle("hidden");
         }
         break;
       case "backing-option-3":
         pledgeBox2.classList.toggle("hidden");
-        if (!pledgeBox1.hasClass("hidden")) {
+        if (!pledgeBox1.classList.contains("hidden")) {
           pledgeBox1.classList.toggle("hidden");
         }
         break;
     }
   }
-  circles.forEach(circle => {
-    if (circle.id === event.target.id) {
-      return;
-    } else {
-      circle.className = "circle";
-      circle.parentNode.parentNode.classList.remove("thick-cyan-border");
-      circle.classList.remove("filled-circle");
-    }
-  });
 }
 
 const windowOnClick = (event) => {
@@ -68,7 +77,11 @@ const toggleBookmarked = () => {
     bookmarkImgInner.setAttribute('fill', "#B1B1B1");
     bookmarkText.innerText = "Bookmark";
   }
-    
+}
+
+const toggleThankYou = () => {
+  optionsModal.classList.toggle("hidden");
+  thanksModal.classList.toggle("hidden");
 }
 
 trigger.addEventListener("click", toggleModal);
@@ -76,3 +89,5 @@ closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 circles.forEach(circle => circle.addEventListener("click", toggleSelection));
 bookmarkButton.addEventListener("click", toggleBookmarked);
+continueButtons.forEach(button => button.addEventListener("click", toggleThankYou));
+okayButton.addEventListener("click", toggleModal);
